5701650e117bd4859dedff89ce536af62d90c3a7
Shawn Rice <iamshawnrice@gmail.com>
2016-01-29 09:29:16 -0500

    Continue down Authentication rabbithole

--- src/common/requestTransform.js
+++ src/common/requestTransform.js
@@ -2,12 +2,8 @@ angular.module('App.common').service('requestTransform', function($cookies) {
   return {
     request: function(config) {
       config.headers = config.headers || {};
-      //Assume that you store the token in a cookie.
-      var globals = $cookies.get( 'globals' ) || {};
+      config.headers.Authorization = 'Bearer ' + $cookies.get('authToken');
 
-      config.headers['Authorization'] = 'Bearer'
-
-      console.log(config);
       return config;
     }
   }
--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -2,7 +2,7 @@ angular.module('App.components').component('loginForm', {
   bindings: {},
   templateUrl: '/templates/loginForm.html',
   controllerAs: 'loginForm',
-  controller: function($scope, api) {
+  controller: function($scope, api, $cookies) {
     $scope.user = {
       username: '',
       password: ''
@@ -12,6 +12,8 @@ angular.module('App.components').component('loginForm', {
       api.auth.save({}, $scope.user, function(response) {
         console.log('SUCCESS!');
         console.log(response);
+
+        $cookies.put('authToken', response.token);
       }, function(response) {
         console.log('ERROR!');
         console.log(response);
