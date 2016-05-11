a77836ed03a1d0721a7e9d5009ce3d5b4dfa69a1
Shawn Rice <iamshawnrice@gmail.com>
2016-05-11 17:32:29 -0400

    Build out HTML for intro component

---
+++ src/components/intro/intro.js
@@ -0,0 +1,13 @@
+var $ = require('jquery');
+var transitions = require('../../common/transitions');
+
+require('imports?$=jquery!../../vendors/velocity');
+
+angular.module('App.components').component('intro', {
+  bindings: {},
+  templateUrl: '/templates/intro.html',
+  controllerAs: 'intro',
+  controller: function($scope) {
+    debugger;
+  }
+});
