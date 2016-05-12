40670ea9a8b36c962ddcb2dfe7c4109363ee9b10
Shawn Rice <iamshawnrice@gmail.com>
2016-05-11 23:47:30 -0400

    first draft of initial animation sequence complete

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,13 +1,25 @@
 var $ = require('jquery');
-var transitions = require('../../common/transitions');
+var transition = require('../../common/transitions').vertSlideDown;
 
 require('imports?$=jquery!../../vendors/velocity');
+require('imports?$=jquery!../../vendors/velocity.ui');
 
 angular.module('App.components').component('intro', {
   bindings: {},
   templateUrl: '/templates/intro.html',
   controllerAs: 'intro',
   controller: function($scope) {
-    debugger;
+    $('[class^=intro]').velocity({translateY: '-20px'}, 0);
+
+    var revealSequence = [
+      {e: $('.intro-1'), p: {translateY: 0, opacity: 1}, o: {duration: 400}},
+      {e: $('.intro-2'), p: {translateY: 0, opacity: 1}, o: {duration: 400, delay: 800}},
+      {e: $('.intro-3'), p: {translateY: 0, opacity: 1}, o: {duration: 400, delay: 1200}},
+      {e: $('.intro-4'), p: {translateY: 0, opacity: 1}, o: {duration: 400, delay: 800}},
+      {e: $('.intro-5'), p: {translateY: 0, opacity: 1}, o: {duration: 400, delay: 1600}},
+      {e: $('.intro-6'), p: {translateY: 0, opacity: 1}, o: {duration: 800}}
+    ];
+
+    $.Velocity.RunSequence(revealSequence);
   }
 });
